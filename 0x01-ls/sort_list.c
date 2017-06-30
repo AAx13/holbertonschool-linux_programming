3fda519b1b195fd8af1d21bf72164a1b30b25a41
DamianAli <damian.ali33@gmail.com>
2017-06-30 15:32:16 -0700

    testing sort_list.c on server.

--- 0x01-ls/sort_list.c
+++ 0x01-ls/sort_list.c
@@ -10,25 +10,22 @@
  */
 void sort_list(dirlist *head, int dir, int file)
 {
-	dirlist *node, *next_node, *prev_node;
+	dirlist *node, *next_node;
 
 	node = head;
-	prev_node = NULL;
-	while (node && _strncmp(node->name, ".", 1) != 0)
+	while (node)
 	{
 		next_node = node->next;
-		if (node->next && _strcasecmp(node->name, next_node->name) > 0)
+		if (node->next
+		    && _strcasecmp(node->name, next_node->name) > 0
+		    && _strcmp(node->name, ".") != 0
+		    && _strcmp(node->name, "..") != 0)
 		{
-			if (prev_node)
-			{
-				prev_node->next = next_node;
-			}
-			node->next = next_node->next;
-			next_node->next = node;
+			printf("HI sorting\n");
 		}
-		prev_node = node;
 		node = node->next;
 	}
+
 	file++;
 	dir++;
 }

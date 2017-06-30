0be2f703aa10aaa57a9fa95c377f1290d708be26
DamianAli <damian.ali33@gmail.com>
2017-06-30 14:43:02 -0700

    updated sort_list.c. testing on server.

--- 0x01-ls/sort_list.c
+++ 0x01-ls/sort_list.c
@@ -10,16 +10,23 @@
  */
 void sort_list(dirlist *head, int dir, int file)
 {
-	dirlist *node, *next_node;
+	dirlist *node, *next_node, *prev_node;
 
 	node = head;
+	prev_node = NULL;
 	while (node && _strncmp(node->name, ".", 1) != 0)
 	{
 		next_node = node->next;
 		if (node->next && _strcasecmp(node->name, next_node->name) > 0)
 		{
-			printf("Swappin %s and %s\n", node->name, next_node->name);
+			if (prev_node)
+			{
+				prev_node->next = next_node;
+			}
+			node->next = next_node->next;
+			next_node->next = node;
 		}
+		prev_node = node;
 		node = node->next;
 	}
 	file++;

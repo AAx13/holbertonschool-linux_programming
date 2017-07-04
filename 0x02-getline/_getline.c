0188b68420f423c553874b84f4f4a5f7e8a052c5
DamianAli <damian.ali33@gmail.com>
2017-07-03 17:23:19 -0700

    testing task 0 checker.

--- 0x02-getline/_getline.c
+++ 0x02-getline/_getline.c
@@ -48,3 +48,47 @@ char *_getline(const int fd)
 	free(line_read);
 	return (NULL);
 }
+
+/**
+ * truncate_line - breaks off correct line to be returned.
+ * @line_read: Original line to be truncated.
+ *
+ * Return: Truncated string or NULL if error allocating memory.
+ */
+char *truncate_line(char *line_read)
+{
+	char *line_out;
+	size_t len;
+
+	len = _strlen(line_read) + 1;
+	line_out = malloc(sizeof(char) * len);
+	if (!line_out)
+	{
+		perror("line_out");
+		return (NULL);
+	}
+
+	strncpy(line_out, line_read, len);
+
+	return (line_out);
+
+}
+
+/**
+ * _strlen - gets the length of a string excluding the null byte.
+ * @str: A string.
+ *
+ * Return: Length of a string.
+ */
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+
+	return (i);
+}

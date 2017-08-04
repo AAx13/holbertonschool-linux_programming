ebdb813da9cfd5a3a731d26cfb421d83e8809fde
DamianAli <damian.ali33@gmail.com>
2017-08-04 01:44:42 -0700

    initial push and testing task 0 against hbtn checks.

---
+++ 0x05-signals/0-handle_signal.c
@@ -0,0 +1,28 @@
+#include "signals.h"
+
+/**
+ * print_sig - prints a signal number.
+ * @sig: A signal number.
+ *
+ * Return: Void.
+ */
+void print_sig(int sig)
+{
+	printf("Gotcha! [%d]\n", sig);
+}
+
+/**
+ * handle_signal - set a handler for the signal SIGINT.
+ *
+ * Return: 0 if successful or -1 on failure.
+ */
+int handle_signal(void)
+{
+	signal(SIGINT, print_sig);
+	if (errno)
+	{
+		return (-1);
+	}
+
+	return (0);
+}
---
+++ 0x05-signals/signals.h
@@ -0,0 +1,12 @@
+#ifndef _SIG_H_
+#define _SIG_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <errno.h>
+
+int handle_signal(void);
+
+#endif /* _SIG_H_ */

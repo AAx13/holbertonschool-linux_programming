fd108b269c805490c1264a30661a6277ada49e1e
DamianAli <damian.ali33@gmail.com>
2017-08-18 18:03:38 -0700

    running task 7 against hbtn checks.

---
+++ 0x05-signals/7-signal_send.c
@@ -0,0 +1,20 @@
+#include "signals.h"
+
+/**
+ * main - sends the signal SIGINT to a process, given its PID.
+ * @ac: Number of arguments.
+ * @av: Array of arguments passed.
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE.
+ */
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		printf("Usage: %s <pid>", av[0]);
+		return (EXIT_FAILURE);
+	}
+	kill(atoi(av[1]), SIGINT);
+
+	return (EXIT_SUCCESS);
+}

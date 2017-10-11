5bfb5c3262f71a3cad5551fe61c13e2ceb8b4053
DamianAli <damian.ali33@gmail.com>
2017-10-11 00:10:52 -0700

    Initial commit task 0 is finished.

---
+++ 0x09-libasm/0-strlen.asm
@@ -0,0 +1,18 @@
+BITS 64
+
+	global asm_strlen
+
+	section .text
+
+	;; Copy of strlen function in C
+
+asm_strlen:
+
+	mov	rcx, -1			; Initialize with -1 incase empty strings are passed.
+loop:
+	inc     rcx			; Increment counter register.
+	cmp     BYTE [rdi + rcx], 0	; Compare register holding string at the position of the rcx counter val.
+	jne	loop			; Continue loop if not null.
+
+	mov	rax, rcx		; Return the counters value.
+	ret
---
+++ 0x09-libasm/libasm.h
@@ -0,0 +1,6 @@
+#ifndef _ASM_H_
+#define _ASM_H_
+
+size_t asm_strlen(const char *str);
+
+#endif /* _ASM_H_ */

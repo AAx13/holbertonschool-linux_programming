41a9ec045ccb876bfd2c5f6c4300d1bf42919476
DamianAli <damian.ali33@gmail.com>
2017-10-12 16:28:33 -0700

    finished task 2. commented.

--- 0x09-libasm/2-strncmp.asm
+++ 0x09-libasm/2-strncmp.asm
@@ -7,3 +7,33 @@ BITS 64
 	;; Copycat of the function strncmp(3), in x86-64 Assembly
 
 asm_strncmp:
+
+	mov	cx, -1
+	mov	rax, 0
+
+loop:
+	
+	inc	cx			; Starting from zero we compare counter to size_t n passed.
+	cmp	cx, dx
+	je	end			; Jump to end if counter is equal to size_t n passed.
+
+	mov	al, BYTE [rdi + rcx] 	; Move next character into BYTE rax register.
+	sub	al, BYTE [rsi + rcx]	; Subtract that character from the next character in 2nd string.
+	cmp	al, 0			; Compare the result to 0
+	jl	lesser			; If less than - jump to lesser label modify value to -1 and return.
+	jg	greater			; If greater than - jump to greater label modify value to 1 and return.
+	jmp	loop
+
+lesser:
+
+	mov	eax, -1
+	jmp	end
+
+greater:
+	
+	mov	eax, 1
+	jmp	end
+	
+end:
+
+	ret

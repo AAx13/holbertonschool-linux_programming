cd36fb28af9576902ed1d090b9b6d78a4baec58b
DamianAli <damian.ali33@gmail.com>
2017-10-11 18:49:37 -0700

    pushing task 1 strcmp.

--- 0x09-libasm/1-strcmp.asm
+++ 0x09-libasm/1-strcmp.asm
@@ -8,4 +8,32 @@ BITS 64
 
 asm_strcmp:
 
-	
+	mov	rax, rdi
+	cmp	BYTE [rax], 0
+	je	lesser
+	cmp	BYTE [rsi], 0
+	je	greater
+	sub	rax, rsi
+
+	cmp	rax, 0
+	jl	lesser
+	jg	greater
+	je	equal
+
+lesser:
+
+	mov	rax, -1
+	jmp	end
+
+greater:
+
+	mov	rax, 1
+	jmp	end
+
+equal:
+
+	mov	rax, 0
+	jmp	end
+
+end:
+	ret

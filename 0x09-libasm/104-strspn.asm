3a84163b1eaffa9e4fb4a7238585fb8e60e0919d
DamianAli <damian.ali33@gmail.com>
2017-10-22 15:08:10 -0700

    Finished task 10.

---
+++ 0x09-libasm/104-strspn.asm
@@ -0,0 +1,40 @@
+BITS 64
+
+	global	asm_strspn
+
+	section	.text
+
+	;; Copycat of the function strspn(3), in x86-64 Assembly.
+
+asm_strspn:
+
+	xor	rax, rax		; Zero rax rbx and r9 for use later.
+	xor	rbx, rbx
+	xor	r9, r9
+	mov	rcx, -1			; Initialize rcx and rdx to -1 for use as counters.
+	mov	rdx, -1
+
+loop:
+
+	inc	rcx
+	mov	bl, BYTE [rdi + rcx] 	; Iterate first string to check character by character.
+	cmp	bl, 0
+	je	end
+
+accept_loop:
+
+	inc	rdx
+	mov	r9b, BYTE [rsi + rdx] 	; Nested loop to search for accepted characters.
+	cmp	r9b, 0
+	je	end
+
+	cmp	bl, r9b			; Compare *S1 to *S2 if not equal continue to iterate S2
+	jne	accept_loop 
+
+	inc	rax			; Else increment rax reset rdx to -1 and jump to outer loop.
+	mov	rdx, -1
+	jmp	loop
+
+end:
+
+	ret
